16e9b71fd904b408c2215018ac95da6d9c1df35d
Steven Dahdah <steven.nfd+gh@gmail.com>
2019-04-24 14:27:41 -0400

    Add Armijo rule for step size, allow Problem cost function to be returned

--- opt.py
+++ opt.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Problem:
     """Optimization problem"""
 
@@ -5,8 +8,34 @@ class Problem:
         self._cost = cost
         self._grad = grad
 
-    def cost(self, x):
-        return self._cost(x)
+    def cost(self, x=None):
+        if x is not None:
+            return self._cost(x)
+        else:
+            return self._cost
 
-    def grad(self, x):
-        return self._grad(x)
+    def grad(self, x=None):
+        if x is not None:
+            return self._grad(x)
+        else:
+            return self._grad
+
+
+def _step_size(p, x, s, gamma=1.5, mu=0.8):
+    """Armijo algorithm for computing step size"""
+
+    k_g = 0 # Power of gamma
+    k_m = 0 # Power of mu
+
+    def v_bar(w):
+        return p.cost(x) + 0.5 * w * p.grad(x) @ s
+
+    while p.cost(x + gamma**k_g * s) < v_bar(gamma**k_g):
+        k_g += 1
+        w = gamma**k_g
+
+    while p.cost(x + mu**k_m * gamma**k_g * s) > v_bar(mu**k_m * gamma**k_g):
+        k_m += 1
+        w = mu**k_m * gamma**k_g
+
+    return w
--- tests.py
+++ tests.py
@@ -2,23 +2,39 @@ import unittest
 import numpy as np
 import opt
 
+np.set_printoptions(precision=20)
+
+
 class TestProblem(unittest.TestCase):
 
     def test_scalar_problem(self):
-        v = lambda x : x * x
-        del_v = lambda x : 2 * x
+        v = lambda x: x * x
+        del_v = lambda x: 2 * x
         p = opt.Problem(v, del_v)
         self.assertEqual(p.cost(-2), 4)
         self.assertEqual(p.grad(-2), -4)
 
     def test_multivar_problem(self):
         P = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 4]])
-        v = lambda x : 0.5 * x.T @ P @ x
-        del_v = lambda x : x.T @ P
+        v = lambda x: 0.5 * x.T @ P @ x
+        del_v = lambda x: x.T @ P
         p = opt.Problem(v, del_v)
         self.assertEqual(p.cost(np.array([[1], [1], [1]])), 3.5)
         self.assertSequenceEqual(p.grad(np.array([[1], [1], [1]])).tolist(),
                                  np.array([[1, 2, 4]]).tolist())
+
+    def test_step_size(self):
+        P = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 4]])
+        v = lambda x: 0.5 * x.T @ P @ x
+        del_v = lambda x: x.T @ P
+        p = opt.Problem(v, del_v)
+        # Compare step sizes at (1, 1, 1)
+        x = np.array([[1], [1], [1]])
+        s = -p.grad(x).T
+        w_ideal = -(p.grad(x) @ s) / (s.T @ P @ s)
+        w_armijo = opt._step_size(p, x, s)
+        self.assertTrue(abs(w_armijo - w_ideal) / w_ideal < 0.1)
+
 
 if __name__ == '__main__':
     unittest.main()

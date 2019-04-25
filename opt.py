Fix bug in conjugate gradient, speed up Armijo

opt.py
@@ -8,7 +8,7 @@ class Problem:
     def __init__(self, cost, grad=None, grad_step=None):
         self._cost = cost
         if grad is None and grad_step is None:
-            self._grad_step = 1e-6
+            self._grad_step = 1e-8
             self._grad = partial(_fd_grad, self, h=self._grad_step)
         elif grad is None and grad_step is not None:
             self._grad_step = grad_step
@@ -49,8 +49,8 @@ def conjugate_gradient(p, x, tolerance=1e-6):
         w = _step_size(p, x, s)
         x_prv = x
         x = x_prv + w * s
-        beta = (p.grad(x) - p.grad(x_prv)) @ p.grad(x).T \
-            / p.grad(x_prv) @ p.grad(x_prv).T
+        beta = ((p.grad(x) - p.grad(x_prv)) @ p.grad(x).T) \
+            / (p.grad(x_prv) @ p.grad(x_prv).T)
         s = -p.grad(x).T + beta * s
 
     return x
@@ -84,8 +84,12 @@ def _step_size(p, x, s, gamma=1.5, mu=0.8):
     k_g = 0  # Power of gamma
     k_m = 0  # Power of mu
 
+    # Precompute cost and gradient to save time
+    vx = p.cost(x)
+    gx = p.grad(x)
+
     def v_bar(w):
-        return p.cost(x) + 0.5 * w * p.grad(x) @ s
+        return vx + 0.5 * w * gx @ s
 
     while p.cost(x + gamma**k_g * s) < v_bar(gamma**k_g):
         k_g += 1
@@ -102,7 +106,7 @@ def _fd_grad(p, x, h=1e-6):
     """Finite difference approximation of the gradient"""
 
     dim = np.max(np.shape(x))
-    grad = ((p.cost(x + h * np.eye(dim)[:, [i]]) - p.cost(x)) / h
-            for i in range(0, dim))
-
-    return np.expand_dims(np.fromiter(grad, np.float), axis=0)
+    grad_gen = ((p.cost(x + h * np.eye(dim)[:, [i]]) - p.cost(x)) / h
+               for i in range(0, dim))
+    grad = np.expand_dims(np.fromiter(grad_gen, np.float), axis=0)
+    return grad

profl.py
@@ -20,7 +20,7 @@ class ProfileProblemA(unittest.TestCase):
                           [4,  7,  0,  1, 21, 15],
                           [7,  5,  7,  9, 15, 27]])
         v = lambda x : a + b.T @ x + 0.5 * x.T @ C @ x
-        self.p = opt.Problem(v)
+        self.p = opt.Problem(v, grad_step=1e-8)
         self.x_opt = -np.linalg.solve(C, b)
         self.pr = cProfile.Profile()
         self.pr.enable()
@@ -33,17 +33,29 @@ class ProfileProblemA(unittest.TestCase):
 
     def test_sd_p(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.steepest_descent(self.p, x, tolerance=1e-3)
+        x_opt = opt.steepest_descent(self.p, x, tolerance=1e-6)
+        print('sd')
+        print(x_opt)
+        print(self.x_opt)
+        print()
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
     def test_cg_p(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.conjugate_gradient(self.p, x, tolerance=1e-3)
+        x_opt = opt.conjugate_gradient(self.p, x, tolerance=1e-6)
+        print('cg')
+        print(x_opt)
+        print(self.x_opt)
+        print()
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
     def test_sec_p(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.secant(self.p, x, tolerance=1e-3)
+        x_opt = opt.secant(self.p, x, tolerance=1e-6)
+        print('sec')
+        print(x_opt)
+        print(self.x_opt)
+        print()
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
 

tests.py
@@ -55,18 +55,18 @@ class TestProblemA(unittest.TestCase):
 
     def test_sd(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.steepest_descent(self.p, x, tolerance=1e-3)
-        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
+        x_opt = opt.steepest_descent(self.p, x, tolerance=1e-6)
+        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-6)
 
     def test_cg(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.conjugate_gradient(self.p, x, tolerance=1e-3)
-        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
+        x_opt = opt.conjugate_gradient(self.p, x, tolerance=1e-6)
+        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-6)
 
     def test_sec(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
-        x_opt = opt.secant(self.p, x, tolerance=1e-3)
-        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
+        x_opt = opt.secant(self.p, x, tolerance=1e-6)
+        self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-6)
 
 
 class TestProblemB(unittest.TestCase):
@@ -78,17 +78,17 @@ class TestProblemB(unittest.TestCase):
         self.p = opt.Problem(v)
 
     def test_sd(self):
-        x = np.array([[10], [10]])
+        x = np.array([[2], [2]])
         x_opt = opt.steepest_descent(self.p, x, tolerance=1e-4)
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
     def test_cg(self):
-        x = np.array([[10], [10]])
+        x = np.array([[2], [2]])
         x_opt = opt.conjugate_gradient(self.p, x, tolerance=1e-4)
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
     def test_sec(self):
-        x = np.array([[10], [10]])
+        x = np.array([[2], [2]])
         x_opt = opt.secant(self.p, x, tolerance=1e-4)
         self.assertTrue(np.linalg.norm(x_opt - self.x_opt) < 1e-3)
 
3bae46dea7a81a2196c064350bc1f6f29e0b361a
Steven Dahdah <steven.nfd+gh@gmail.com>
2019-04-26 20:20:58 -0400

    Add timing to demo, along with new figures

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import opt
@@ -24,7 +25,9 @@ class TestProblemAGrad(unittest.TestCase):
 
     def test_sd(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.steepest_descent(self.p, x, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -34,10 +37,13 @@ class TestProblemAGrad(unittest.TestCase):
         fig.savefig('./fig/sd-pA-grad.eps', format='eps')
         print('\nProblem A, Steepest Descent (Exact Gradient)')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_cg(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.conjugate_gradient(self.p, x, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -47,10 +53,13 @@ class TestProblemAGrad(unittest.TestCase):
         fig.savefig('./fig/cg-pA-grad.eps', format='eps')
         print('\nProblem A, Conjugate Gradient (Exact Gradient)')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_sec(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.secant(self.p, x, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -60,6 +69,7 @@ class TestProblemAGrad(unittest.TestCase):
         fig.savefig('./fig/sec-pA-grad.eps', format='eps')
         print('\nProblem A, Secant (Exact Gradient)')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
 
 class TestProblemA(unittest.TestCase):
@@ -80,7 +90,9 @@ class TestProblemA(unittest.TestCase):
 
     def test_sd(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.steepest_descent(self.p, x, tol=1e-6, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -90,10 +102,13 @@ class TestProblemA(unittest.TestCase):
         fig.savefig('./fig/sd-pA.eps', format='eps')
         print('\nProblem A, Steepest Descent')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_cg(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.conjugate_gradient(self.p, x, tol=1e-6, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -103,10 +118,13 @@ class TestProblemA(unittest.TestCase):
         fig.savefig('./fig/cg-pA.eps', format='eps')
         print('\nProblem A, Conjugate Gradient')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_sec(self):
         x = np.array([[0], [0], [0], [0], [0], [0]])
+        start = time.time()
         x_opt = opt.secant(self.p, x, tol=1e-6, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -116,6 +134,7 @@ class TestProblemA(unittest.TestCase):
         fig.savefig('./fig/sec-pA.eps', format='eps')
         print('\nProblem A, Secant')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
 
 class TestProblemB(unittest.TestCase):
@@ -128,7 +147,9 @@ class TestProblemB(unittest.TestCase):
 
     def test_sd(self):
         x = np.array([[10], [10]])
+        start = time.time()
         x_opt = opt.steepest_descent(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -138,10 +159,13 @@ class TestProblemB(unittest.TestCase):
         fig.savefig('./fig/sd-pB.eps', format='eps')
         print('\nProblem B, Steepest Descent')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_cg(self):
         x = np.array([[10], [10]])
+        start = time.time()
         x_opt = opt.conjugate_gradient(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -151,10 +175,13 @@ class TestProblemB(unittest.TestCase):
         fig.savefig('./fig/cg-pB.eps', format='eps')
         print('\nProblem B, Conjugate Gradient')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_sec(self):
         x = np.array([[10], [10]])
+        start = time.time()
         x_opt = opt.secant(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -164,6 +191,7 @@ class TestProblemB(unittest.TestCase):
         fig.savefig('./fig/sec-pB.eps', format='eps')
         print('\nProblem B, Secant')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
 
 class TestProblemC(unittest.TestCase):
@@ -179,7 +207,9 @@ class TestProblemC(unittest.TestCase):
 
     def test_sd(self):
         x = np.array([[0], [0]])
+        start = time.time()
         x_opt = opt.steepest_descent(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -189,10 +219,13 @@ class TestProblemC(unittest.TestCase):
         fig.savefig('./fig/sd-pC.eps', format='eps')
         print('\nProblem C, Steepest Descent')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_cg(self):
         x = np.array([[0], [0]])
+        start = time.time()
         x_opt = opt.conjugate_gradient(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -202,10 +235,13 @@ class TestProblemC(unittest.TestCase):
         fig.savefig('./fig/cg-pC.eps', format='eps')
         print('\nProblem C, Conjugate Gradient')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_sec(self):
         x = np.array([[0], [0]])
+        start = time.time()
         x_opt = opt.secant(self.p, x, tol=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         fig = plt.figure()
@@ -215,6 +251,7 @@ class TestProblemC(unittest.TestCase):
         fig.savefig('./fig/sec-pC.eps', format='eps')
         print('\nProblem C, Secant')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
 
 class TestProblemD(unittest.TestCase):
@@ -228,7 +265,9 @@ class TestProblemD(unittest.TestCase):
 
     def test_penalty_function(self, tol=1e-3, tol_const=1e-3):
         x0 = np.array([[0], [0]])
+        start = time.time()
         x_opt = opt.penalty_function(self.p, x0, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         c_e = np.array([np.linalg.norm(self.p.eq_const(x_opt[i]))
@@ -245,10 +284,13 @@ class TestProblemD(unittest.TestCase):
         fig.savefig('./fig/pe-pD.eps', format='eps')
         print('\nProblem D, Penalty Function')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     def test_inv_barrier_function(self, tol=1e-3, tol_const=1e-3):
         x0 = np.array([[0.1], [0.1]])
+        start = time.time()
         x_opt = opt.barrier_function(self.p, x0, mode='inv', hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         c_e = np.array([np.linalg.norm(self.p.eq_const(x_opt[i]))
@@ -265,6 +307,7 @@ class TestProblemD(unittest.TestCase):
         fig.savefig('./fig/ba-pD.eps', format='eps')
         print('\nProblem D, Barrier Function')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     @unittest.skip('Log barrier function does not work correctly')
     def test_log_barrier_function(self, tol=1e-3, tol_const=1e-3):
@@ -273,13 +316,18 @@ class TestProblemD(unittest.TestCase):
 
     def test_aug_lag(self):
         x0 = np.array([[0.1], [0.1]])
+        start = time.time()
         x_opt = opt.augmented_lagrange(self.p, x0, tol=1e-4, tol_const=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
+            for i in range(len(x_opt))])
+        c_e = np.array([np.linalg.norm(self.p.eq_const(x_opt[i]))
             for i in range(len(x_opt))])
         c_i = np.array([np.linalg.norm(np.minimum(self.p.ineq_const(x_opt[i]), 0))
             for i in range(len(x_opt))])
         fig = plt.figure()
         plt.plot(np.arange(len(x_opt)), g, label='Gradient Norm')
+        plt.plot(np.arange(len(x_opt)), c_e, label='Equality Constraint Norm')
         plt.plot(np.arange(len(x_opt)), c_i, label='Inequality Constraint Norm')
         plt.xticks(np.arange(len(x_opt)))
         plt.xlabel('Iteration')
@@ -287,11 +335,14 @@ class TestProblemD(unittest.TestCase):
         fig.savefig('./fig/al-pD.eps', format='eps')
         print('\nProblem D, Augmented Lagrange')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     @unittest.skip('Wrong answer')
     def test_lag_new(self):
         x0 = np.array([[0.7], [0.7]])
+        start = time.time()
         x_opt = opt.lagrange_newton(self.p, x0, tol=1e-4)
+        end = time.time()
         print(x_opt)
 
 
@@ -305,7 +356,9 @@ class TestProblemE(unittest.TestCase):
 
     def test_penalty_function(self, tol=1e-4, tol_const=1e-4):
         x0 = np.array([[1], [1]])
+        start = time.time()
         x_opt = opt.penalty_function(self.p, x0, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         c_i = np.array([np.linalg.norm(np.minimum(self.p.ineq_const(x_opt[i]), 0))
@@ -319,11 +372,14 @@ class TestProblemE(unittest.TestCase):
         fig.savefig('./fig/pe-pE.eps', format='eps')
         print('\nProblem E, Penalty Function')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     @unittest.skip('Does not terminate ... ?')
     def test_inv_barrier_function(self, tol=1e-4, tol_const=1e-4):
         x0 = np.array([[1], [1]])
+        start = time.time()
         x_opt = opt.barrier_function(self.p, x0, mode='inv', hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         c_i = np.array([np.linalg.norm(np.minimum(self.p.ineq_const(x_opt[i]), 0))
@@ -337,6 +393,7 @@ class TestProblemE(unittest.TestCase):
         fig.savefig('./fig/ba-pE.eps', format='eps')
         print('\nProblem E, Barrier Function')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     @unittest.skip('Log barrier function does not work correctly')
     def test_log_barrier_function(self, tol=1e-4, tol_const=1e-4):
@@ -345,7 +402,9 @@ class TestProblemE(unittest.TestCase):
 
     def test_aug_lag(self):
         x0 = np.array([[1], [1]])
+        start = time.time()
         x_opt = opt.augmented_lagrange(self.p, x0, tol=1e-4, tol_const=1e-4, hist=True)
+        end = time.time()
         g = np.array([np.linalg.norm(self.p.grad(x_opt[i]))
             for i in range(len(x_opt))])
         c_i = np.array([np.linalg.norm(np.minimum(self.p.ineq_const(x_opt[i]), 0))
@@ -359,6 +418,7 @@ class TestProblemE(unittest.TestCase):
         fig.savefig('./fig/al-pE.eps', format='eps')
         print('\nProblem E, Augmented Lagrange')
         print('arg min v(x) =\n', x_opt[-1])
+        print('time =\n', end - start, 's')
 
     @unittest.skip('Wrong answer')
     def test_lag_new(self):
